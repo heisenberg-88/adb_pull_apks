5dc27551f5bb3eb0f738ddfc4886ef1ff0ed901c
Heisenberg <75937169+heisenberg-88@users.noreply.github.com>
2021-07-23 16:24:11 +0530

    minor changes

--- script.bat
+++ script.bat
@@ -1,3 +1,14 @@
+@echo Enter your pairing ip:port 
+@echo off
+set /P id=ip:port : 
+adb pair %id%
+
+@echo Enter your connecting ip:port 
+@echo off
+set /P idd=ip:port : 
+adb connect %idd%
+@echo connecting.......
+
 @echo Starting.......
 @echo off
 adb shell pm list packages > pkgslist.txt
@@ -17,4 +28,7 @@ python %k%\pathStrip.py
 cd ..
 FOR /F "tokens=* delims=" %%x in (finalPaths.txt) DO adb pull %%x %k%\apks
 @echo apks pulled successfully...
+del pkgslist.txt
+del adbpath.txt
+adb disconnect %idd%
 pause
